ba58720da52b0dfe02077fa80bb1b354c3c76982
TomatoCakePasta <gaomuk50@gmail.com>
2024-08-21 19:12:16 +0900

    dbと接続

--- copla-vue/src/components/HomeContent.vue
+++ copla-vue/src/components/HomeContent.vue
@@ -18,13 +18,16 @@
     const chatContent = ref("");
     const postId = ref(1);
     const repId = ref(1);
-    const getMsg = ref("");
+    const getMsg = ref();
 
     // const postsImageData = ref([]);
 
     // データの取得はこんなイメージ?
     // 各投稿の中で返信を入れ子で持つ
-    const postsImageData = ref([
+
+    const postsImageData = ref();
+    /*
+    = ref([
         { id: postId.value++, userName: "Taro", content: "Hello1\nこんにちは", replies: 
             [
                 {id: repId.value++, userName: "Taro", content: "返信"}, 
@@ -55,6 +58,7 @@
             ] 
         },
     ]);
+    */
 
     const samplePost = ref({ id: postId.value++, userName: "Taro", content: "Hello1", replies: 
             [
@@ -83,37 +87,76 @@
         else {
             alert("Write something");
         }
-    }
+    };
 
     // 投稿を取得
     // 画面読み込み時, 一定間隔, ソケットイベント検知などのタイミングで呼出
     const getDatas = () => {
         // 以下のURLに投稿取得リクエストをします
-        axios.get("/getaaa")
+        axios.get("http://localhost:3000/get/all")
             .then((res) => {
-                // 処理が成功した場合
-                // postImageDataに入れてPostContentコンポーネントに渡します
+                postsImageData.value = nestPostsAndReplies(res.data.posts);
+                console.log(postsImageData);
+                console.log("GET DATA");
             })
             .catch((err) => {
                 console.error(err);
             });
-    }
+    };
 
     // ジャンル選択
     const onSearch = (word) => {
         console.log(`${ word }の投稿を表示`);
 
         // axiosでリクエスト送る?
-    }
+    };
 
-    const onTest = () => {
-        console.log("GET Request");
-        axios.get("http://localhost:3000/get")
-            .then((res) => {
-                getMsg.value = res.data.msg;
-                console.log(getMsg);
-                console.log("GET DATA");
-            })
+    function nestPostsAndReplies(data) {
+        // 投稿IDをキーにして、投稿とその返信を格納するマップ
+        const postsMap = new Map();
+
+        // 元のデータをループして、投稿とその返信をグループ化する
+        data.forEach(item => {
+            // 投稿がまだマップにない場合は追加
+            if (!postsMap.has(item.postID)) {
+                postsMap.set(item.postID, {
+                    postID: item.postID,
+                    postName: item.postName,
+                    postContent: item.postContent,
+                    // postUserIcon: 0,
+                    postFav: item.postFav,
+                    postTime: item.postTime,
+                    postGenre: item.genre,
+
+                    // 投稿を送った人のidを追記
+                    // postUserId: item.postUserId,
+
+                    // 学系学部学年とか
+                    // position: item.position,
+                    // department: item.department,
+                    // location: item.location,
+
+                    // lastEnterTime: item.lastEnterTime,
+                    replies: []  // 返信の配列
+                });
+            }
+            
+            // 投稿に対する返信を追加
+            if (item.repName) {
+                postsMap.get(item.postID).replies.push({
+                    repID: item.repID,
+                    repName: item.repName,
+                    repContent: item.repContent,
+                    // repUserIcon: item.repUserIcon,
+                    repTime: item.repTime,
+                    // 返信を送った人のidを追記
+                    // repUserId: item.repUserId
+                });
+            }
+        });
+
+        // マップから配列に変換
+        return Array.from(postsMap.values());
     }
 
 </script>
@@ -128,9 +171,6 @@
             します。JSのonClick属性みたいな感じ? addEventListenerを使う事も減ると思います
         -->
         <div>
-            <v-btn @click="onTest">GET</v-btn>
-            {{ getMsg }}
-            あとで見た目は整えます。とりあえず架空のD大学(D University)にしておきます
             <v-card
                 class="ma-5 my-2"
                 elevation="2"
@@ -172,7 +212,7 @@
             投稿のデザインの型はPostContentコンポーネントに作っているので
             値を渡しています
         -->
-        <div v-for="post in postsImageData" :key="post.id">
+        <div v-for="post in postsImageData" :key="post.postID">
             <!-- Flagで投稿コンポーネントと記事コンポーネントを区別する? -->
 
             <!-- 投稿の場合 -->
@@ -180,8 +220,9 @@
                 propという変数でコンポーネントに値を渡します
                 左辺に任意の変数名を指定し、右辺に今回のfor文で取得したpostを渡します
             -->
+            {{ post }}
             <PostContent
-                :key="post.id"
+                :key="post.postID"
                 :post="post"
             />
 
