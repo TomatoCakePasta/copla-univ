5206a9eeb56ab4fd2affbcdcde4c2c99fe339b13
TomatoCakePasta <gaomuk50@gmail.com>
2024-09-16 16:51:09 +0900

    時刻表のAPIをControllerとModelに分割

--- backend/controllers/transitTimeController.js
+++ backend/controllers/transitTimeController.js
@@ -1,7 +1,18 @@
 import { 
-
+        fetchTransitTimetable
     } from "../models/transitTimeModel.js";
 
+// 時刻表を取得
 export const getTransitTimetable = (req, res) => {
-
+    console.log("/transit getTransitTimetable");
+    
+    fetchTransitTimetable((err, timetable) => {
+        if (err) {
+            console.error("Failed to fetch timetable", err);
+            res.send({ flag: false });
+        }
+        else {
+            res.status(200).send({ flag: true, timetable: timetable });
+        }
+    });
 };
--- backend/models/transitTimeModel.js
+++ backend/models/transitTimeModel.js
@@ -1,1 +1,19 @@
 import con from "../etc/db.js";
+
+export const fetchTransitTimetable = (callback) => {
+    const query = `SELECT 
+                        station,
+                        dest,
+                        depTime,
+                        endTime,
+                        schedule
+                    FROM bustime
+                    ORDER BY
+                        station,
+                        dest,
+                        depTime
+                    ASC
+                    `;
+    
+    con.query(query, callback);
+}
