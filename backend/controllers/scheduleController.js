ed8862d1b557831a0ffc2033fb3219b708a581d9
TomatoCakePasta <gaomuk50@gmail.com>
2024-09-16 16:41:42 +0900

    時間割のAPIをControllerとModelに分割

--- backend/controllers/scheduleController.js
+++ backend/controllers/scheduleController.js
@@ -1,11 +1,39 @@
 import { 
-
+        getTimeTableByUserId,
+        updateTimetableByUsreId
     } from "../models/scheduleModel.js";
 
+// 時間割取得
 export const getUserTimetable = (req, res) => {
+    const userID = req.session.user.userID;
 
+    console.log("get /schedule getUserTimetable");
+
+    getTimeTableByUserId(userID, (err, timetable) => {
+        if (err) {
+            console.error("Failed to get timetable by userID", err);
+            res.send({ flag: false });
+        }
+        else {
+            res.status(200).send({ flag: true, timetable: timetable });
+        }
+    });
 };
 
+// 時間割更新
 export const updateUserTimetable = (req, res) => {
+    const userID = req.session.user.userID;
+    const timetable = req.body.classes;
+    
+    console.log("post /schedule updateUserTimetable");
 
+    updateTimetableByUsreId(userID, timetable, (err) => {
+        if (err) {
+            console.error("Failed to update timetable by userID", err);
+            res.send({ flag: false });
+        }
+        else {
+            res.status(200).send({ flag: true });
+        }
+    });
 };
